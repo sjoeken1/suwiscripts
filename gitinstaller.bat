4378dba35a32099fcfdc021893847eb8b16efd03
sjoeken1 <sjoekennumber@proton.me>
2024-10-30 11:31:21 +0100

    beschrijf hier de veranderingen

--- gitinstaller.bat
+++ gitinstaller.bat
@@ -3,5 +3,4 @@ mkdir gitinstaller
 cd gitinstaller
 curl -LO https://github.com/git-for-windows/git/releases/download/v2.47.0.windows.2/Git-2.47.0.2-64-bit.exe
 start Git-2.47.0.2-64-bit.exe
-pause 
 exit /b
